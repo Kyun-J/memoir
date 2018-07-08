379ffe2bd414d2aad6599952183519320b4eb61b
Kyun-J <dvkyun@gmail.com>
2018-07-08 19:11:57 +0900

    what

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const express = requrie('express')
+const express = require('express')
 const app = express()
 
 app.listen(8008)
+
+app.use(express.static(__dirname));
+
+app.get('/',(req,res,next) => {
+  res.sendFile(__dirname+'/src/index.html')
+})
